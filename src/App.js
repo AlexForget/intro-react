44db068c7e15f99c1103b3ae3131bfad6400f083
Alex Forget <alexandreqc26@gmail.com>
2021-09-13 15:18:37 -0400

    Asynchronous React et testing

--- src/App.js
+++ src/App.js
@@ -4,8 +4,57 @@ import restaurant from "./restaurant.jpg";
 
 
 
+// -------------Test------------------------
+
+function App() {
+
+  return (
+    <div>
+      <h1>React testing library</h1>
+    </div>
+  )
+}
+
+
+// -------------Fetching data with hook------------------------
+
+
+/*
+function App({ login }) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!login) return;
+    setLoading(true);
+    fetch(`https://api.github.com/users/${login}`)
+      .then((response) => response.json())
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, [login]);
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>
+  if (!data) return null;
+
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.location}</p>
+      <img alt={data.login} src={data.avatar_url}></img>
+    </div>
+  )
+}
+*/
+
+
+
 // -------------useReducer------------------------
 
+/*
 function App() {
 
   const [checked, toggle] = useReducer(
@@ -23,7 +72,7 @@ function App() {
     </>
   )
 }
-
+*/
 
 
 
